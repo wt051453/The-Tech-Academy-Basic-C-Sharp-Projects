31e42f01a958119a73846e79f62634213bfbe91e
wt051453 <wt051453@gmail.com>
2018-09-08 09:58:46 -0700

    Accounting Software exercise

---
+++ Accounting.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Accounting
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Rememeber, we're \"accounting\" on you!");
+            Console.ReadLine();
+        }
+    }
+}
