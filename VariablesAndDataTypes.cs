a77019f1baebef71d7df121165f9394157793010
wt051453 <wt051453@gmail.com>
2018-09-10 20:27:23 -0700

    resubmit Variable and Data Types exercise.

--- VariablesAndDataTypes.cs
+++ VariablesAndDataTypes.cs
@@ -11,17 +11,15 @@ namespace VariablesAndDataTypes
             Console.WriteLine("What course are you on?");
             string whatCourse = Console.ReadLine();
             Console.WriteLine("What page number?");
-            string pageNumber = Console.ReadLine();
-            int pgNum = Convert.ToInt32(pageNumber);
+            int pgNum = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\".");
-            string needHelp = Console.ReadLine();
+            bool needHelp = Console.ReadLine() == "true";
             Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
             string positiveExperience = Console.ReadLine();
             Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
             string otherFeedback = Console.ReadLine();
             Console.WriteLine("How many hours did you study today?");
-            string studyHours = Console.ReadLine();
-            int studyHr = Convert.ToInt32(studyHours);
+            int studyHr = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
             Console.ReadLine();
         }
