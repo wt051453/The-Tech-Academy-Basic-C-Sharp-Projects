cd251cd226e12d50eca9a3b0a73cde24a848295a
wt051453 <wt051453@gmail.com>
2018-10-16 23:26:05 -0700

    Interface exercise on page 126.

---
+++ StudentName2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentName2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee n = new Employee();
+            n.SayName();
+            n.Quit();
+        }
+    }
+}
