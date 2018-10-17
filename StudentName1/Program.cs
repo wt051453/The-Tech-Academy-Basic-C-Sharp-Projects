b3f683cc6675dee2c85535a0938cddb6d7cc7adf
wt051453 <wt051453@gmail.com>
2018-10-16 22:10:43 -0700

    Student Name Exercise on page 124.

---
+++ StudentName1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentName1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee n = new Employee();
+            n.SayName();
+        }
+    }
+}
