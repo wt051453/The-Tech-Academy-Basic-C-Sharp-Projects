312c84158f85326616b47a60f21cc9eb4930f9de
wt051453 <wt051453@gmail.com>
2018-10-20 16:18:50 -0700

    Overload Operator == Exercise

---
+++ OverloadOperator/OverloadOperator/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadOperator
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public static bool operator ==(Employee x, Employee y)
+        {
+            return x.Id == y.Id;
+        }
+
+        public static bool operator !=(Employee x, Employee y)
+        {
+            return x.Id != y.Id;
+        }
+
+        public override bool Equals(object obj)
+        {
+            Employee Id = obj as Employee;
+            if (Id != null)
+            {
+                return Id == this;
+            }
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
---
+++ OverloadOperator/OverloadOperator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee n = new Employee();
+            n.Id = 1001;
+
+            Employee n1 = new Employee();
+            n.Id = 1002;
+
+            Console.WriteLine(n == n1);
+            Console.ReadLine();
+            
+        }
+    }
+}
