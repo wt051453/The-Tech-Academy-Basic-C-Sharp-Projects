b468fff89ecd6877c279d3a3d8b8ce2dd1bf491a
wt051453 <wt051453@gmail.com>
2018-10-27 16:56:19 -0700

    Lambda exercise on page 140

---
+++ LambdaExercise/LambdaExercise/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>();
+            employees.Add(new Employee { FirstName = "Joe", LastName = "Green", Id = 1 });
+            employees.Add(new Employee { FirstName = "Joe", LastName = "Yellow", Id = 2 });
+            employees.Add(new Employee { FirstName = "John", LastName = "Smith", Id = 3 });
+            employees.Add(new Employee { FirstName = "John", LastName = "Doe", Id = 4 });
+            employees.Add(new Employee { FirstName = "James", LastName = "Davidson", Id = 5 });
+            employees.Add(new Employee { FirstName = "David", LastName = "Johnson", Id = 6 });
+            employees.Add(new Employee { FirstName = "Johnny", LastName = "Rogers", Id = 7 });
+            employees.Add(new Employee { FirstName = "Jason", LastName = "Hawkins", Id = 8 });
+            employees.Add(new Employee { FirstName = "Fanny", LastName = "May", Id = 9 });
+            employees.Add(new Employee { FirstName = "Jane", LastName = "Doe", Id = 10 });
+
+            foreach (var employee in employees)
+            {
+
+                if (employee.FirstName == "Joe")
+                {
+                    List<Employee>Templist = new List<Employee>();
+                    Templist.Add(employee);
+                    Console.WriteLine(employee.FirstName + employee.LastName + employee.Id);
+                }
+            }
+
+            List<Employee> joe = employees.Where(x => x.FirstName == "Joe").ToList();
+
+            foreach (Employee employee in joe)
+            {
+                Console.WriteLine(employee.FirstName);
+            }
+
+            List<Employee> GreatThanFive = employees.Where(x => x.Id > 5).ToList();
+
+            foreach (Employee employee in GreatThanFive)
+            {
+                Console.WriteLine(employee.FirstName + employee.LastName + employee.Id);
+            }
+            Console.ReadLine();
+        }
+    }
+}
