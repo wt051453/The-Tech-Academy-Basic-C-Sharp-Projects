def8338af267cec7f5736f1af16c4ff7ad5a0065
wt051453 <wt051453@gmail.com>
2018-10-27 11:30:07 -0700

    Enum exercise on page 134

---
+++ Enum/Enum/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumExercise
+{
+    
+    class Program
+    {
+        enum DaysOfWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the current day of the week.");
+            string userInput = Console.ReadLine();
+
+            DaysOfWeek Day;
+
+            try
+            {
+                Day = (DaysOfWeek)Enum.Parse(typeof(DaysOfWeek), userInput);
+                Console.WriteLine("Today is " + Day);
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Please enter an actual day of the week");
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+        
+    }
+}
