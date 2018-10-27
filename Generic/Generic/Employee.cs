5efa8a8a88b972a07441e2c53fe569eab866c740
wt051453 <wt051453@gmail.com>
2018-10-26 21:53:20 -0700

    Generic exercise on page 131

---
+++ Generic/Generic/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic
+{
+    public class Employee<T> : Person
+    {
+        public List<T> things { get; set; }
+
+        public override void SayName()
+        {
+            string fullName = firstName + lastName;
+            Console.WriteLine("Name: " + fullName);
+            Console.ReadLine();
+        }
+        public Employee()
+        {
+            firstName = "Sample";
+            lastName = "Student";
+        }
+    }
+}
---
+++ Generic/Generic/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> n = new Employee<string>();
+            n.things = new List<string>() { "Employee Identification", "Office Space", "Office Supplies", "Computer" };
+            Employee<int> m = new Employee<int>();
+            m.things = new List<int>() { 1, 1, 3, 1 };
+
+            foreach (string thing in n.things)
+            {
+                Console.WriteLine(thing);
+                Console.ReadLine();
+            }
+
+            foreach (int thing in m.things)
+            {
+                Console.WriteLine(thing);
+                Console.ReadLine();
+            }
+        }
+    }
+}
