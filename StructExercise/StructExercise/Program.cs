1bc4ac7e0721b3a1c70a8cc583ee3c2764577397
wt051453 <wt051453@gmail.com>
2018-10-27 11:56:52 -0700

    Struct exercise on page 137

---
+++ StructExercise/StructExercise/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number n = new Number();
+            n.Amount = 10.003M;
+            Console.WriteLine(n.Amount);
+            Console.ReadLine();
+        }
+    }
+}
