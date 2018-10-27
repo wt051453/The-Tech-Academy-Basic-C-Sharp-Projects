2f43e53a436e01e7ea723e5a8eb791d7ac2f1d3a
wt051453 <wt051453@gmail.com>
2018-10-26 19:55:52 -0700

    Interface exercise on page 126

---
+++ StudentName2/StudentName2/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentName2
+{
+    public class Employee : Person, IQuittable
+    {
+        public Employee()
+        {
+            FirstName = "Sample";
+            LastName = "Student";
+        }
+
+        public void Quit()
+        {
+            Console.WriteLine("The above employee is quitting the job today.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StudentName2/StudentName2/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentName2
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public void SayName()
+        {
+            string FullName = FirstName + LastName;
+            Console.WriteLine("Name: " + FullName);
+            Console.ReadLine();
+        }
+    }
+}
