9f994f8024c3f86997e444a929a130a83516e34d
wt051453 <wt051453@gmail.com>
2018-10-31 23:17:36 -0700

    IO exercise on page 154

---
+++ IOexercise/IOexercise/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOexercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number.");
+            string text = Console.ReadLine();
+            File.WriteAllText(@"C:\Users\wt051453\Desktop\Folder1\Logs\log.txt", text);
+            using (StreamReader file = File.OpenText(@"C:\Users\wt051453\Desktop\Folder1\Logs\log.txt"))
+            {
+                string number = "";
+
+                while ((number = file.ReadLine())!= null)
+                {
+                    Console.WriteLine(number);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
