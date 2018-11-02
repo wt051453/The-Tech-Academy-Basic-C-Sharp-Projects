23ec7a8dc05b50d24cca820acce152855c213e3a
wt051453 <wt051453@gmail.com>
2018-11-01 21:01:12 -0700

    DateTime exercise on page 156

---
+++ DateTime/DateTime/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime currentTime = DateTime.Now;
+            Console.WriteLine(currentTime);
+
+            Console.WriteLine("Please enter a number.");
+            var hours = Convert.ToInt32(Console.ReadLine());
+
+            DateTime timeInHours = currentTime.AddHours(hours);
+            Console.WriteLine(timeInHours);
+            Console.ReadLine();
+            
+        }
+    }
+}
