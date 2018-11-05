7c2f5a5751cef555a15152e2b14b21924ff8e260
wt051453 <wt051453@gmail.com>
2018-11-04 16:53:18 -0800

    C# additional features exercise on page 162

---
+++ AdditionalFeatures/AdditionalFeatures/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionalFeatures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int currentYear = 2018;
+            Console.WriteLine("The current year is {0}", currentYear);
+
+            var previousYear = new Time("previous Year", 2017);
+            Console.WriteLine("The previous year is {0}", previousYear.Year);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AdditionalFeatures/AdditionalFeatures/Time.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionalFeatures
+{
+    class Time
+    {
+        public string Name { get; set; }
+        public int Year { get; set; }
+
+        public Time (string name, int year)
+        {
+            Name = name;
+            Year = year;
+        }
+        public Time() : this("Two years ago", 2016)
+        {
+        }
+    }
+}
